e095e813afd0271e0ca15fbcc51671143ba03653
Brian Schroeder <bts@ersc.io>
2025-10-17 13:27:19 -0400

    index: make {Readonly,Mutable}Index::change_id_index fallible
    This is part of a series of changes to make most methods on index traits
    (i.e. `ChangeIdIndex`, `MutableIndex`, `ReadonlyIndex`, `Index`)
    fallible. This will enable networked implementations of these traits,
    which may produce I/O errors during operation. See #7825 for more
    information.

--- lib/tests/test_index.rs
+++ lib/tests/test_index.rs
@@ -1205,7 +1205,8 @@ fn test_change_id_index() {
             .mutable_index()
             .change_id_index(&mut commits.iter().map(|commit| commit.id()))
     };
-    let change_id_index = index_for_heads(&[&commit_1, &commit_2, &commit_3, &commit_4, &commit_5]);
+    let change_id_index =
+        index_for_heads(&[&commit_1, &commit_2, &commit_3, &commit_4, &commit_5]).unwrap();
     let prefix_len = |commit: &Commit| {
         change_id_index
             .shortest_unique_prefix_len(commit.change_id())
@@ -1258,7 +1259,7 @@ fn test_change_id_index() {
     // Test with an index containing only some of the commits. The shortest
     // length doesn't have to be minimized further, but unreachable commits
     // should never be included in the resolved set.
-    let change_id_index = index_for_heads(&[&commit_1, &commit_2]);
+    let change_id_index = index_for_heads(&[&commit_1, &commit_2]).unwrap();
     let resolve_prefix = |prefix: &str| {
         change_id_index
             .resolve_prefix(&HexPrefix::try_from_hex(prefix).unwrap())
